d897f1809cd0acc2b9a697d11baedbd9d6edfa7b
Dave O <davidobrite@gmail.com>
2013-06-29 16:31:55 -0700

    added .gitconfig to load.sh

--- load.sh
+++ load.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/.dircolors ~/.dircolors
 ln -s ~/dotfiles/.bash_prompt.sh ~/.bash_prompt.sh
 ln -s ~/dotfiles/.xmonad ~/
 ln -s ~/dotfiles/.xmobarrc ~/
+ln -s ~/dotfiles/.gitconfig ~/
