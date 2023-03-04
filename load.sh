d1413cb36eece33882f569a852199c18b8bb50f3
Dave O <david@planningcenter.com>
2023-03-04 06:36:28 -0800

    update load with gitconfig instructions

--- load.sh
+++ load.sh
@@ -2,3 +2,5 @@ ln -s ~/dotfiles/.dircolors ~/.dircolors
 ln -s ~/dotfiles/.gitconfig ~/
 ln -s ~/dotfiles/.aliases ~/
 ln -s ~/dotfiles/nvim ~/.config
+ln -s ~/dotfiles/.gitconfig ~/
+cp .gitconfig.local ~ # then fill out file
