aa55a596a4632a6d4650ff783016593c9c89a174
Dave O <davidobrite@gmail.com>
2023-03-04 10:05:33 -0800

    Cleanup unused arg

--- nvim/init.lua
+++ nvim/init.lua
@@ -217,7 +217,7 @@ require('telescope').setup {
   },
   pickers = {
     live_grep = {
-      additional_args = function(opts)
+      additional_args = function()
         return { '--sort-files' }
       end,
     },
