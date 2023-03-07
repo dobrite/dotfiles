014f720f43a1d23cff5f178ee830773587a8db3b
David Obrite <david@planningcenter.com>
2023-03-07 06:42:59 -0800

    Keyboard shortcuts for null-ls functions

--- nvim/lua/custom/plugins/null-ls.lua
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -1,3 +1,6 @@
+vim.cmd 'map <Leader>ln :NullLsInfo<CR>'
+vim.cmd 'map <Leader>lf :lua vim.lsp.buf.format({ timeout_ms = 2000 })<CR>'
+
 return {
   'jose-elias-alvarez/null-ls.nvim',
   config = function()
