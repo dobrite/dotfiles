b101354734941056757c8d623bcc619f47045326
David Obrite <david@planningcenter.com>
2023-03-07 06:43:11 -0800

    Rubocop via bundled rubocop

--- nvim/lua/custom/plugins/null-ls.lua
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -9,6 +9,10 @@ return {
     null_ls.setup {
       sources = {
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.diagnostics.rubocop.with {
+          command = 'bundle',
+          args = vim.list_extend({ 'exec', 'rubocop' }, null_ls.builtins.diagnostics.rubocop._opts.args),
+        },
       },
     }
   end,
