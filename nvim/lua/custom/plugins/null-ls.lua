79844eb2dd06a9920c4e1b77c51099a33244b86d
David Obrite <david@planningcenter.com>
2023-03-08 05:19:56 -0800

    Guard rubocop around having the rubocop gem

--- nvim/lua/custom/plugins/null-ls.lua
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -1,20 +1,42 @@
 vim.cmd 'map <Leader>ln :NullLsInfo<CR>'
 vim.cmd 'map <Leader>lf :lua vim.lsp.buf.format({ timeout_ms = 2000 })<CR>'
 
+local M = {}
+function M.installed_via_bundler(gemname)
+  local gemfile_lock = vim.fn.getcwd() .. '/Gemfile.lock'
+
+  if vim.fn.filereadable(gemfile_lock) == 0 then
+    return
+  end
+
+  local found = false
+  for line in io.lines(gemfile_lock) do
+    if string.find(line, gemname) then
+      found = true
+      break
+    end
+  end
+
+  return found
+end
+
 return {
   'jose-elias-alvarez/null-ls.nvim',
   config = function()
     local null_ls = require 'null-ls'
-
-    null_ls.setup {
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.diagnostics.rubocop.with {
-          command = 'bundle',
-          args = vim.list_extend({ 'exec', 'rubocop' }, null_ls.builtins.diagnostics.rubocop._opts.args),
-        },
-      },
+    local sources = {
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettierd,
     }
+
+    if M.installed_via_bundler 'rubocop' then
+      local rubocop_source = null_ls.builtins.diagnostics.rubocop.with {
+        command = 'bundle',
+        args = vim.list_extend({ 'exec', 'rubocop' }, null_ls.builtins.diagnostics.rubocop._opts.args),
+      }
+
+      vim.list_extend(sources, { rubocop_source })
+    end
+    null_ls.setup { sources = sources }
   end,
 }
