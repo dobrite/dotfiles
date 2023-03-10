775157732e9eabaa5d23610baff885e2f434e133
Dave O <davidobrite@gmail.com>
2023-03-10 07:18:27 -0800

    Add autopep8 formatting via null-ls

--- nvim/lua/custom/plugins/null-ls.lua
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -25,6 +25,7 @@ return {
   config = function()
     local null_ls = require 'null-ls'
     local sources = {
+      null_ls.builtins.formatting.autopep8,
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.prettierd,
     }
