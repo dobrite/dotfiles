f176c87fb36604f3c6881799cc43d1d2a38d911b
Dave O <davidobrite@gmail.com>
2023-03-25 05:46:04 -0700

    feat(fzf): add fzf

--- load.sh
+++ load.sh
@@ -3,4 +3,5 @@ ln -s ~/dotfiles/.aliases ~
 ln -s ~/dotfiles/.zshrc ~
 ln -s ~/dotfiles/.gitconfig ~
 ln -s ~/dotfiles/.commitlintrc.js ~
+ln -s ~/dotfiles/.fzf.zsh ~
 cp ~/dotfiles/.gitconfig.local ~ # then fill out file
