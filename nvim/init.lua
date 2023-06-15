ccea8d2078c04bac969a540c08b65ffeebbd4273
Dave O <davidobrite@gmail.com>
2023-06-15 07:28:16 -0700

    fix(fidget): point to legacy tag

--- nvim/init.lua
+++ nvim/init.lua
@@ -47,7 +47,7 @@ require('lazy').setup({
       'simrat39/rust-tools.nvim',
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
     },
   },
 
