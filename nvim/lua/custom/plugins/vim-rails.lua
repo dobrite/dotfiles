87bb8c898caf98d22ca481f53c58e0021c4983a1
Dave O <davidobrite@gmail.com>
2024-05-10 07:21:13 -0700

    feat(rails): define more alternate files for

--- nvim/lua/custom/plugins/vim-rails.lua
+++ nvim/lua/custom/plugins/vim-rails.lua
@@ -10,6 +10,16 @@ vim.cmd [[
         \     "app/graphs/app_graph/vertices/{}_vertex.rb"
         \   ],
         \ },
+        \ "app/graphs/app_graph/edges/*_edge.rb": {
+        \   "test": [
+        \     "spec/graphs/app_graph/latest/edges/{}_edge_spec.rb"
+        \   ],
+        \ },
+        \ "spec/graphs/app_graph/latest/edges/*_edge_spec.rb": {
+        \   "alternate": [
+        \     "app/graphs/app_graph/edges/{}_edge.rb"
+        \   ],
+        \ },
         \ "app/graphs/church_center_graph/vertices/*_vertex.rb": {
         \   "test": [
         \     "spec/graphs/church_center_graph/latest/vertices/{}_vertex_spec.rb"
@@ -20,6 +30,16 @@ vim.cmd [[
         \     "app/graphs/church_center_graph/vertices/{}_vertex.rb"
         \   ],
         \ },
+        \ "app/graphs/church_center_graph/edges/*_edge.rb": {
+        \   "test": [
+        \     "spec/graphs/church_center_graph/latest/edges/{}_edge_spec.rb"
+        \   ],
+        \ },
+        \ "spec/graphs/church_center_graph/latest/edges/*_edge_spec.rb": {
+        \   "alternate": [
+        \     "app/graphs/church_center_graph/edges/{}_edge.rb"
+        \   ],
+        \ },
         \ "app/graphs/feature_graph/vertices/*_vertex.rb": {
         \   "test": [
         \     "spec/graphs/feature_graph/latest/vertices/{}_vertex_spec.rb"
@@ -28,6 +48,16 @@ vim.cmd [[
         \ "spec/graphs/feature_graph/latest/vertices/*_vertex_spec.rb": {
         \   "alternate": [
         \     "app/graphs/feature_graph/vertices/{}_vertex.rb"
+        \   ],
+        \ },
+        \ "app/graphs/feature_graph/edges/*_edge.rb": {
+        \   "test": [
+        \     "spec/graphs/feature_graph/latest/edges/{}_edge_spec.rb"
+        \   ],
+        \ },
+        \ "spec/graphs/feature_graph/latest/edges/*_edge_spec.rb": {
+        \   "alternate": [
+        \     "app/graphs/feature_graph/edges/{}_edge.rb"
         \   ],
         \ }}
 ]]
