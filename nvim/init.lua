dcd396213ba6f93bf12b3429029ed4bd859eb8a7
Dave O <davidobrite@gmail.com>
2025-01-10 11:31:48 -0800

    chore(conform): enable syntax_tree for ruby

--- nvim/init.lua
+++ nvim/init.lua
@@ -995,7 +995,7 @@ require('conform').setup {
   formatters_by_ft = {
     lua = { 'stylua' },
     -- python = { 'isort', 'black' },
-    ruby = { 'rubocop' },
+    ruby = { 'syntax_tree', 'rubocop' },
     rust = { 'rustfmt', lsp_format = 'fallback' },
     javascript = { 'prettierd' },
     javascriptreact = { 'prettierd' },
