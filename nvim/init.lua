b49370e40d51582ae260de29d9b635c840a3e841
Dave O <davidobrite@gmail.com>
2025-06-27 06:28:35 -0700

    fix(lualine): we want relative from vim working folder

--- nvim/init.lua
+++ nvim/init.lua
@@ -192,7 +192,7 @@ require('lazy').setup({
       sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'diff', 'diagnostics' },
-        lualine_c = { { 'filename', path = 2 } },
+        lualine_c = { { 'filename', path = 1 } },
       },
     },
   },
